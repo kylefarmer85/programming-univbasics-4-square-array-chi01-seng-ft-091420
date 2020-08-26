4e6d03e2e44156aa746b2587950eda7bdfcdccb3
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-26 19:38:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,14 @@
+# def square_array(numbers)
+#   new_array = []
+#   numbers.length.times { |i|
+#   new_array.push(numbers[i] ** 2)
+#   }
+#   return new_array
+# end
+
 def square_array(numbers)
   new_array = []
-  numbers.length.times { |i|
-  new_array.push(numbers[i] ** 2)
-  }
-  return new_array
+  while numbers.count { |i| new_array.push(numbers[i]** 2) }
+  end
+new_array
 end
